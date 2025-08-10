ec2c48a52cef5de044c1676c8c994753ffc48ad9
CodeWithManas <manas865873@gmail.com>
2025-08-10 23:08:11 +0530

    first commit

---
+++ src/main/java/com/codewithmanas/gobooleanbackend/GobooleanbackendApplication.java
@@ -0,0 +1,13 @@
+package com.codewithmanas.gobooleanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobooleanbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GobooleanbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithmanas/gobooleanbackend/GobooleanbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmanas.gobooleanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GobooleanbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

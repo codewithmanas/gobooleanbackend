47611846b215d6b16252bc5d8fd1b57da3b0984f
CodeWithManas <manas865873@gmail.com>
2025-08-16 01:01:35 +0530

    add global exception handler

--- src/main/java/com/codewithmanas/gobooleanbackend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/codewithmanas/gobooleanbackend/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,46 @@
 package com.codewithmanas.gobooleanbackend.common.exception;
 
+import com.codewithmanas.gobooleanbackend.common.util.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<List<String>>>  handleValidationException(MethodArgumentNotValidException ex, HttpServletRequest httpServletRequest) {
+
+        String path = httpServletRequest.getRequestURI();
+        String requestId = UUID.randomUUID().toString();
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + " ; " + error.getDefaultMessage())
+                .toList();
+
+        log.warn("[requestId={}] Invalid  request", requestId);
+
+        ApiResponse<List<String>> response = new ApiResponse<>(
+                400,
+                "Invalid Request",
+                null,
+                errors,
+                requestId,
+                path
+        );
+
+        return ResponseEntity.badRequest().body(response);
+    }
+
+
 }
